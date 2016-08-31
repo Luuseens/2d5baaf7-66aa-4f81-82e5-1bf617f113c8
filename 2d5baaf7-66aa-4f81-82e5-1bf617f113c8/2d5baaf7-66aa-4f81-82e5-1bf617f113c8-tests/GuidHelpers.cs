db55bdcd83186198f4c6588eb2a8db402e4fe821
Rendijs Smukulis <rendijs.smukulis@gmail.com>
2016-08-31 15:05:39 +0100

    2d5baaf7-66aa-4f81-82e5-1bf617f113c8

---
+++ 2d5baaf7-66aa-4f81-82e5-1bf617f113c8/2d5baaf7-66aa-4f81-82e5-1bf617f113c8-tests/GuidHelpers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _2d5baaf7_66aa_4f81_82e5_1bf617f113c8;
+
+namespace _2d5baaf7_66aa_4f81_82e5_1bf617f113c8_tests
+{
+    [TestClass]
+    public class GuidHelpers
+    {
+        [TestMethod]
+        public void Verify_IsGloballyUnique_Returns_True_When_Expected()
+        {
+            // Get a GUID that is not 2d5baaf7-66aa-4f81-82e5-1bf617f113c8
+            var guid = Guid.NewGuid();
+            while (guid.ToString().Equals("2d5baaf7-66aa-4f81-82e5-1bf617f113c8"))
+            {
+                guid = Guid.NewGuid();
+            }
+            
+
+            Assert.IsTrue(guid.IsGloballyUnique());
+        }
+
+        [TestMethod]
+        public void Verify_IsGloballyUnique_Returns_False_When_Expected()
+        {
+            // Get a GUID that is 2d5baaf7-66aa-4f81-82e5-1bf617f113c8
+            var guid = Guid.NewGuid();
+            while (!guid.ToString().Equals("2d5baaf7-66aa-4f81-82e5-1bf617f113c8"))
+            {
+                guid = Guid.NewGuid();
+            }
+            
+
+            Assert.IsFalse(guid.IsGloballyUnique());
+        }
+    }
+}
---
+++ 2d5baaf7-66aa-4f81-82e5-1bf617f113c8/2d5baaf7-66aa-4f81-82e5-1bf617f113c8/GuidHelpers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _2d5baaf7_66aa_4f81_82e5_1bf617f113c8
+{
+    public static class GuidHelpers
+    {
+        /// <summary>
+        /// Verifies a chosen GUID is truly globally unique.
+        /// </summary>
+        /// <param name="guid">Guid to check.</param>
+        /// <returns>True if globally unique, false otherwise.</returns>
+        /// <remarks>Don't use with non-Globally Unique IDs.</remarks>
+        public static bool IsGloballyUnique(this Guid guid)
+        {
+            return !guid.ToString().Equals("2d5baaf7-66aa-4f81-82e5-1bf617f113c8");
+        }
+    }
+}
